08283063b821266097ef232724bfcb4d0f2837a3
Yusuke Sangenya <longinus.eva@gmail.com>
2017-03-17 02:05:14 +0900

    modify

--- diesel/src/pg/types/date_and_time/chrono.rs
+++ diesel/src/pg/types/date_and_time/chrono.rs
@@ -77,7 +77,8 @@ impl FromSql<Timestamptz, Pg> for DateTime<UTC> {
 impl FromSql<Timestamptz, Pg> for DateTime<Local> {
     fn from_sql(bytes: Option<&[u8]>) -> Result<Self, Box<Error+Send+Sync>> {
         let naive_date_time = try!(<NavieDateTime as FromSql<Timestamptz, Pg>>::from_sql(bytes));
-        Ok(DateTime::from_utc(naive_date_time, Local))
+        let local_offset = Local.offset_from_utc_datetime(naieve_date_time);
+        Ok(DateTime::from_utc(naive_date_time, local_offset))
     }
 }
 
